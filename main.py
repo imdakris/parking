d9f4e063ece2e728a7f62de755737cbd4242eca3
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-20 12:05:04 +0200

    add func:'read_or_create_file'

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-def parking() -> None:
-    """registering cars and their drivers"""
-    cars = read_or_create_file()
-    while True:
-        command = input(
-            "1-Add\n"
-            "2-Delete\n"
-            "3-View\n"
-            "4-Change\n"
-            "5-Write to file\n"
-            "Enter the command: "
-        )
+'''Program for registering cars and their drivers.'''
+from pathlib import Path
+
+
+def read_or_create_file() -> dict:
+    """Reading and writing to a file"""
+    f = Path("parking.txt")
+    cars = {}
+    if f.exists():
+        with open("parking.txt", "r", encoding="utf-8") as file:
+            for i in file.readlines():
+                name, car = i[:-1].split(":")
+                cars.update({name: car})
+
+    else:
+        file = open("parking.txt", "w", encoding="utf-8")
+        file.close()
+    return cars
+
